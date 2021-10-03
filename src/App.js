691d7c58b93d4f04f4f6ddd9824a762a2585f246
Tohir Raihan <tohir.raihan@gmail.com>
2021-10-04 00:29:16 +0600

    add react router and ract bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <div className="App">
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/'>
+                    Home
+                </Route>
+                <Route exact path='/home'>
+                    Home
+                </Route>
+                <Route exact path='/services'>
+                    Services
+                </Route>
+                <Route exact path='/about'>
+                    About
+                </Route>
+                <Route exact path='/blog'>
+                    Blog
+                </Route>
+                <Route path='*'>
+                    404 Not found
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    </div>;
 }
 
 export default App;

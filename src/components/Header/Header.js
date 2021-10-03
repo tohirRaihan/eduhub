62405dc4d41cff535910b5b14ff852ee6625c213
Tohir Raihan <tohir.raihan@gmail.com>
2021-10-04 01:19:54 +0600

    add images to project

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
@@ -7,15 +7,25 @@ const Header = () => {
         <header>
             <Navbar bg="light" expand="lg">
                 <Container>
-                    {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
-                    <Link to='/' className='navbar-brand'>React-Bootstrap</Link>
+                    <Link to="/" className="navbar-brand">
+                        <span className="text-danger fw-light h3">Edu</span>
+                        <span className="text-success fw-bold h2">Hub</span>
+                    </Link>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Link to='/home' className='nav-link'>Home</Link>
-                            <Link to='/about' className='nav-link'>About</Link>
-                            <Link to='/services' className='nav-link'>Services</Link>
-                            <Link to='/blog' className='nav-link'>Blog</Link>
+                        <Nav className="ms-auto">
+                            <Link to="/home" className="nav-link">
+                                Home
+                            </Link>
+                            <Link to="/about" className="nav-link">
+                                About
+                            </Link>
+                            <Link to="/services" className="nav-link">
+                                Services
+                            </Link>
+                            <Link to="/blog" className="nav-link">
+                                Blog
+                            </Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>

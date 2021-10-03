03c7cab58438b92897c4e99719452ce594c4257c
Tohir Raihan <tohir.raihan@gmail.com>
2021-10-04 00:35:12 +0600

    setup the router

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
-    return <div className="App">
-        <BrowserRouter>
-            <Switch>
-                <Route exact path='/'>
-                    Home
-                </Route>
-                <Route exact path='/home'>
-                    Home
-                </Route>
-                <Route exact path='/services'>
-                    Services
-                </Route>
-                <Route exact path='/about'>
-                    About
-                </Route>
-                <Route exact path='/blog'>
-                    Blog
-                </Route>
-                <Route path='*'>
-                    404 Not found
-                </Route>
-            </Switch>
-        </BrowserRouter>
-    </div>;
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Header />
+                <Switch>
+                    <Route exact path="/">
+                        Home
+                    </Route>
+                    <Route exact path="/home">
+                        Home
+                    </Route>
+                    <Route exact path="/services">
+                        Services
+                    </Route>
+                    <Route exact path="/about">
+                        About
+                    </Route>
+                    <Route exact path="/blog">
+                        Blog
+                    </Route>
+                    <Route path="*">404 Not found</Route>
+                </Switch>
+                <Footer/>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;

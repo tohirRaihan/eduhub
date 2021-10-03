7cad8fa8ecfc8d38e88974aae101ff403043e242
Tohir Raihan <tohir.raihan@gmail.com>
2021-10-04 00:43:04 +0600

    add Services component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
+import About from './components/About/About';
 
 function App() {
     return (
@@ -20,7 +21,7 @@ function App() {
                         Services
                     </Route>
                     <Route exact path="/about">
-                        About
+                        <About />
                     </Route>
                     <Route exact path="/blog">
                         Blog
---
+++ src/components/Services/Services.js
@@ -0,0 +1,11 @@
+import './Services.css';
+
+const Services = () => {
+    return (
+        <div>
+            <h1>Services component</h1>
+        </div>
+    );
+}
+
+export default Services;
